bb4f1fed82762c8b9de12b18a22219b8867c673c
Oğuzhan Turan <turan.oguzhan@hotmail.com>
2018-05-30 00:25:55 +0300

    values-band dimen added

--- app/src/main/java/com/udacity/ouz/bakingapp/MainActivity.java
+++ app/src/main/java/com/udacity/ouz/bakingapp/MainActivity.java
@@ -2,15 +2,30 @@ package com.udacity.ouz.bakingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.rv_recipes)
+    RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        //setContentView(R.layout.activity_main);
+        setLayoutColumns();
 
+
+    }
+
+    private void setLayoutColumns(){
+
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this,
+                getResources().getInteger(R.integer.grid_layout_column_num)));
     }
 
 }
